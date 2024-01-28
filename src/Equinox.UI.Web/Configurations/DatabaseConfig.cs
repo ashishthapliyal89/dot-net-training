5ab46cedcb523e91f0e691c347c3cd60c3db64f0
Ashish Thapliyal <ashish.thapliyal@evontech.com>
2024-01-28 12:01:51 +0530

    added new files

---
+++ src/Equinox.UI.Web/Configurations/DatabaseConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Equinox.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Equinox.UI.Web.Configurations
+{
+    public static class DatabaseConfig
+    {
+        public static void AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddDbContextPool<EquinoxContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+            options => options.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: System.TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null)
+                ));
+
+
+            services.AddDbContext<EventStoreSqlContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
